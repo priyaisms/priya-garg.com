b9ccaaff733602a1ba1f8024316849357fa68f35
Priya Garg <priyagarg01@gmail.com>
2020-07-22 10:19:31 -0700

    add blog folder

---
+++ my-blog/content/blog/about.md
@@ -0,0 +1,17 @@
+---
+templateKey: page
+---
+
+Hi, I'm Priya. 
+
+When I was 7 years old, I wrote my first story. It was about three siblings traversing a magical kingdom in search of their long-lost parents. Very _The Boxcar Children_ meets..._Shrek_? Anyways, for the next couple of years, I spent most of my free time hooked, writing stories. None of them were really that great, but I didn't care -- I was doing it for the love of the thing.
+
+As I've grown older, I have found that there are less and less activities I'm doing just for the love of it. I started writing again a couple years ago. Fiction wasn't really doing it for me anymore. Maybe I'll return to it again later, but for now, I'm  enjoying exploring the essay format.
+
+Starting this blog is a way for me to recapture that feeling of loving an activity, and also to improve the discipline of my own thinking. I've found the best way to force myself to understand something is to write about it.
+
+Right now, I'm fascinated by topics like civic tech, the American governmental system, healthcare, science fiction, and consumer product. I hope you'll join me for the ride!
+
+You can find more about my professional background [here](https://www.linkedin.com/in/priya-garg-65b22151/). I also sometimes [tweet](https://twitter.com/priyaisms) and [post on Medium](https://medium.com/@priyaisms_47043).
+
+You can email me at priyagarg01@gmail.com.
---
+++ my-blog/src/pages/index.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import kebabCase from "lodash/kebabCase"
+import { rhythm } from "../utils/typography"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Priya Garg" />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(0 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small style={{textTransform: 'uppercase', color: '#00688B'}}>{node.frontmatter.date} in </small>
+              <small style={{textTransform: 'uppercase', color: '#00688B'}}>
+              <Link to={`/tags/${kebabCase(node.frontmatter.tags)}/`}>
+                #{node.frontmatter.tags}
+              </Link>
+              </small>
+            </header>
+            <section
+            style={{
+              marginTop: rhythm(2 / 4),
+            }}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            tags
+          }
+        }
+      }
+    }
+  }
+`
