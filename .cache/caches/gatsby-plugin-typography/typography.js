a97ec15fb8f754e7046847d897186fed3c94ab4c
Priya Garg <priyagarg01@gmail.com>
2020-07-26 15:21:11 -0700

    add tracking

---
+++ .cache/caches/gatsby-plugin-typography/typography.js
@@ -0,0 +1,1 @@
+module.exports = require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/utils/typography")
---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/pages/index.js"))),
+  "component---src-pages-tags-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/pages/tags.js"))),
+  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/pages/using-typescript.tsx"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/templates/blog-post.js"))),
+  "component---src-templates-page-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/templates/page.js"))),
+  "component---src-templates-tags-js": hot(preferDefault(require("/Users/priyagarg/Desktop/projects/priyaisms.github.io/src/templates/tags.js")))
+}
+
